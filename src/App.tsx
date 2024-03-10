5074b8cfbe6a8fb8d9f25c4f8a2b579eae6b7482
Streltsov Ivan <principdereva@gmail.com>
2024-03-10 13:16:58 +0100

    feat: added logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
+import { useState } from 'react';
 import './App.css';
 import Card from './components/card/card';
 
+import data from './consts/data.json';
+
 function App() {
+  const [state, setState] = useState({
+    lang: 'noLang',
+    category: 'noCategory'
+  });
+  console.log(state);
+  const onCategoryChang = () => {
+
+  }
+
+  const onLanguageChange = () => {
+    //если язык не выбран, то он выбирается
+    //если язык выбран, то он меняется на но лангвидж
+    //если язык и категория выбраны, то язык не меняем, а возвращаемся к категории
+  }
+
+
   return (
-    <div className="App">
-      <div className='flex bg-[#50d71e]'>
-        <Card />
-        <Card />
+    <div className="App flex bg-[#50d71e] h-screen justify-center items-center">
+      <div className="flex gap-x-2">
+        {state.category !== 'noCategory' && 
+          <div>
+            субкатегории
+          </div>
+        }
+
+        {state.lang !== 'noLang' && 
+          <div className='flex'>
+            {
+              data.filter(item => item.lang !== state.lang).map(item => item.categories.map(category => 
+                <Card text={category.category} onCardClick={() => setState({...state, category: category.category})}/>
+              ))
+            }
+          </div>}
+
+        {data.map(item => 
+          <Card text={item.lang} key={item.lang} onCardClick={() => setState({...state, lang: item.lang})}/>
+        )}
       </div>
     </div>
   );
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,3 +1,8 @@
-export default function Card () {
-    return(<div className="m-w-2">123232323232323</div>)
-}
+export default function Card (props: {text: string, onCardClick?: (event: any) => void,  isSecondPlan?: boolean, isThirdPlan?: boolean}) {
+    const {text, onCardClick, isSecondPlan, isThirdPlan} = props;
+    return(
+        <div className="w-[223px] h-[351px] bg-white rounded-xl border-2 border-black p-2" key={text}>
+            <div className="w-[200px] h-[300px]">img</div>
+            <p className="text-center cursor-pointer" onClick={onCardClick}>{ text }</p>
+        </div>
+    )};
