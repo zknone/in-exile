144f0d81403c77ee688f8e8119e6c0aa44acf2a7
Streltsov Ivan <principdereva@gmail.com>
2024-03-10 16:00:57 +0100

    feat: added correct cards render

--- src/App.tsx
+++ src/App.tsx
@@ -9,40 +9,54 @@ function App() {
     lang: 'noLang',
     category: 'noCategory'
   });
-  console.log(state);
-  const onCategoryChang = () => {
+
+  const onCategoryChange = () => {
 
   }
 
-  const onLanguageChange = () => {
-    //если язык не выбран, то он выбирается
-    //если язык выбран, то он меняется на но лангвидж
-    //если язык и категория выбраны, то язык не меняем, а возвращаемся к категории
+  const onLanguageChange = (lang: string) => {
+    if (state.lang !== lang) {
+      setState({...state, lang: lang})
+    } else {
+      if (state.category !== 'noCategory') {
+        setState({...state, category: 'noCategory'});
+      } else {
+        setState({lang: 'noLang', category: 'noCategory'})
+      }
+    };
   }
-
 
   return (
     <div className="App flex bg-[#50d71e] h-screen justify-center items-center">
       <div className="gap-x-2">
         {state.category !== 'noCategory' && 
-          <div>
-            субкатегории
+          <div className="z-20 relative">
+            <div className="absolute flex -top-20 justify-center w-full">
+              {
+                data
+                .filter(item => item.lang !== state.lang)
+                .flatMap(lang => lang.categories)
+                .filter(cat => cat.category === state.category)
+                .flatMap(subc => subc.subcategories.map(subcategory => 
+                  <Card key={subcategory.card} text={subcategory.text}/>)
+                )
+              }
+            </div>
           </div>
         }
         {state.lang !== 'noLang' && 
           <div className="z-10 relative">
-            <div className='absolute flex -top-10 -left-[20%]'>
+            <div className="absolute flex -top-10 justify-center w-full">
               {
                 data.filter(item => item.lang !== state.lang).map(item => item.categories.map(category => 
                   <Card text={category.category} onCardClick={() => setState({...state, category: category.category})}/>
                 ))
               }
             </div>
-
           </div>}
         <div className="flex z-0">
           {data.map(item => 
-            <Card text={item.lang} key={item.lang} onCardClick={() => setState({...state, lang: item.lang})}/>
+            <Card text={item.lang} key={item.lang} onCardClick={() => onLanguageChange(item.lang)}/>
           )}
         </div>
       </div>
