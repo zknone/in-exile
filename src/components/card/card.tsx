9c66cc31531c3e9957889cf2ed2bf79bf95d8fe6
Streltsov Ivan <principdereva@gmail.com>
2024-03-21 20:53:05 +0100

    fix: added opacity on row

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -33,33 +33,37 @@ export default function Card(props: {
 
   return (
     <div
-      ref={cardRef}
+
       className={cn(
         'relative min-w-[223px] h-min-content bg-white rounded-[20px] border-2 border-[#008AFF] p-[6px] shadow-card',
         'hover:bg-[#008AFF] hover:border-[#396E9A] hover:border-1',
         { 'border-[#008AFF]/[0.8]': isSecondPlan },
-        { 'border-[#008AFF]/[0.6]': isThirdPlan }
+        { 'border-[#008AFF]/[0.6]': isThirdPlan  },
       )}
     >
       {isOpen && (
-        <div
-          className="block absolute z-50 -left-[35%] -top-[42%] w-[408px] h-[647px] rounded-[35px] border-2 border-[#FF6CFF] bg-[#FF6CB6] p-2 shadow-card text-xl hover:bg-[#F93598] hover:border-[#B1256C] card"
-          onClick={() => setOpen(false)}
-        >
-          <div className="w-full flex flex-col justify-between h-full border-2 rounded-[25px] border-[#FF6CFF] bg-white hover:border-[#F93598]">
-            <ul className="p-5 my-auto">
-              {points &&
-                points.map((item, index) => (
-                  <li className="pb-5" key={index}>
-                    {item}
-                  </li>
-                ))}
-            </ul>
-            <div className="font-bold text-center py-2 text-2xl">
-              {cardTitle}
+        <>
+          <div className="popup-background"></div>
+          <div
+            ref={cardRef}
+            className="block absolute z-50 -left-[35%] -top-[42%] w-[408px] h-[647px] rounded-[35px] border-2 border-[#FF6CFF] bg-[#FF6CB6] p-2 shadow-card text-xl hover:bg-[#F93598] hover:border-[#B1256C] popup"
+            onClick={() => setOpen(false)}
+          >
+            <div className="popup-content w-full flex flex-col justify-between h-full border-2 rounded-[25px] border-[#FF6CFF] bg-white hover:border-[#F93598] card">
+              <ul className="p-5 my-auto">
+                {points &&
+                  points.map((item, index) => (
+                    <li className="pb-5" key={index}>
+                      {item}
+                    </li>
+                  ))}
+              </ul>
+              <div className="font-bold text-center py-2 text-2xl">
+                {cardTitle}
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
       <div
         className={cn(
@@ -67,7 +71,6 @@ export default function Card(props: {
           { 'border-[#008AFF]/[0.8]': isSecondPlan },
           { 'border-[#008AFF]/[0.6]': isThirdPlan }
         )}
-
         onClick={ openable ? () => setOpen(!isOpen): onCardClick }
       >
         <img className="w-[211px] h-[302px]" src={imgUrl} alt="cardTitle" />
