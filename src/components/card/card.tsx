b37d8b6ae6d929e48f77829c7a5630582529be3a
Streltsov Ivan <principdereva@gmail.com>
2024-04-24 18:01:44 +0200

    fix: radiuses

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -79,15 +79,17 @@
             <div
               ref={cardRef}
               className={cn(
-                'block rounded-[40px] absolute z-50 -left-[35%] p-[11px] -top-[42%] border-[2px] border-[#FF6CFF] bg-[#FF6CB6] p-2 shadow-card hover:bg-[#F93598] hover:border-[#B1256C] popup',
-                {'w-[408px] h-[647px] text-xl ': !isMacAir},
-                {'w-[320px] h-[508px] text-m': isMacAir}
+                'block absolute z-50 -left-[35%] p-[11px] -top-[42%] border-[2px] border-[#FF6CFF] bg-[#FF6CB6] p-2 shadow-card hover:bg-[#F93598] hover:border-[#B1256C] popup',
+                {'w-[408px] h-[647px] rounded-[40px] text-xl ': !isMacAir},
+                {'w-[320px] h-[508px] rounded-[35px] text-l': isMacAir}
               )}
               onClick={() => setOpen(false)}
             >
               <div
                 className={cn(
-                  'popup-content w-full rounded-[30px] flex flex-col justify-between h-full border-[2px] border-[#FF6CFF] bg-white hover:border-[#F93598] card'
+                  'popup-content w-full flex flex-col justify-between h-full border-[2px] border-[#FF6CFF] bg-white hover:border-[#F93598] card',
+                  {'rounded-[30px]': !isMacAir},
+                  {'rounded-[27px]': isMacAir}
                 )}
               >
                 <ul className={cn(
@@ -118,11 +120,11 @@
 
         <div
           className={cn(
-            'flex relative rounded-[20px] border-[2px] shadow-card',
-            { 'w-[154px] p-[2px]': size === 'normal' && isMacAir },
-            { 'w-[223px] p-[6px]': size === 'normal' && !isMacAir },
-            { 'w-[321px] p-[8px] h-[504px]': size === 'big' && !isMacAir },
-            { 'w-[280px] p-[4px] h-[441px]': size === 'big' && isMacAir },
+            'flex relative border-[2px] shadow-card',
+            { 'w-[154px] p-[2px] rounded-[15px] ': size === 'normal' && isMacAir },
+            { 'w-[223px] p-[6px] rounded-[20px]': size === 'normal' && !isMacAir },
+            { 'w-[321px] p-[8px] h-[504px] rounded-[20px]': size === 'big' && !isMacAir },
+            { 'w-[280px] p-[4px] h-[441px] rounded-[20px]': size === 'big' && isMacAir },
             { 'border-[#2FA2FB]': !isHovered && !isNothingHovered },
             'hover:bg-[#008AFF] hover:border-[#396E9A] hover:border-1',
             { 'border-[#008AFF] bg-white': !active },
@@ -141,6 +143,10 @@
               { 'border-[#2FA2FB]': !isHovered && !isNothingHovered },
               { 'border-[#2FA2FB]/[0.7]': isSecondPlan },
               { 'border-[#2FA2FB]/[0.5]': isThirdPlan },
+              { 'rounded-[12px]': size === 'normal' && isMacAir },
+              { 'rounded-[15px]': size === 'normal' && !isMacAir },
+              { 'rounded-[15px]': size === 'big' && !isMacAir },
+              { 'rounded-[15px]': size === 'big' && isMacAir },
             )}
             onClick={openable ? () => setOpen(!isOpen) : onCardClick}
           >
