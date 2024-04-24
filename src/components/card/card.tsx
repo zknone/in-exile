f299742d2833c5caa6b16237e4993e900310ca96
Streltsov Ivan <principdereva@gmail.com>
2024-04-24 14:41:20 +0200

    fix: adaptive widths

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,166 +1,159 @@
-import { MouseEventHandler, useEffect, useRef, useState } from 'react';
-import cn from 'classnames';
+  import { MouseEventHandler, useEffect, useRef, useState } from 'react';
+  import cn from 'classnames';
 
-export default function Card(props: {
-  rotation?: number;
-  cardTitle: string;
-  img: string;
-  points?: string[];
-  onCardClick?: MouseEventHandler<HTMLDivElement>;
-  isSecondPlan?: boolean;
-  isThirdPlan?: boolean;
-  openable?: boolean;
-  active?: boolean;
-  isHovered?: boolean;
-  isNothingHovered?: boolean;
-  altImg?: string;
-  size?: 'normal' | 'big';
-  onMouseEnter?: () => void;
-  onMouseLeave?: () => void;
-}) {
-  const {
-    cardTitle,
-    points,
-    onCardClick,
-    isSecondPlan,
-    isThirdPlan,
-    openable,
-    active,
-    size = 'normal',
-    onMouseEnter,
-    onMouseLeave,
-    isHovered,
-    isNothingHovered,
-    rotation = 0,
-    img,
-    altImg
-  } = props;
-  const [isOpen, setOpen] = useState(false);
-  const cardRef = useRef<HTMLDivElement>(null);
+  export default function Card(props: {
+    rotation?: number;
+    cardTitle: string;
+    img: string;
+    points?: string[];
+    onCardClick?: MouseEventHandler<HTMLDivElement>;
+    isSecondPlan?: boolean;
+    isThirdPlan?: boolean;
+    openable?: boolean;
+    active?: boolean;
+    isHovered?: boolean;
+    isNothingHovered?: boolean;
+    altImg?: string;
+    size?: 'normal' | 'big';
+    onMouseEnter?: () => void;
+    onMouseLeave?: () => void;
+  }) {
+    const {
+      cardTitle,
+      points,
+      onCardClick,
+      isSecondPlan,
+      isThirdPlan,
+      openable,
+      active,
+      size = 'normal',
+      onMouseEnter,
+      onMouseLeave,
+      isHovered,
+      isNothingHovered,
+      rotation = 0,
+      img,
+      altImg
+    } = props;
+    const [isOpen, setOpen] = useState(false);
+    const cardRef = useRef<HTMLDivElement>(null);
 
-  const [isMacAir, setIsMacAir] = useState(false);
+    const [isMacAir, setIsMacAir] = useState(false);
 
-  useEffect(() => {
-    function handleResize() {
-      setIsMacAir(window.innerWidth < 1024);
-    }
+    useEffect(() => {
+      function handleResize() {
+        setIsMacAir(window.innerWidth < 1201);
+      }
 
-    window.addEventListener('resize', handleResize);
+      window.addEventListener('resize', handleResize);
 
-    handleResize();
+      handleResize();
 
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
-  const imgUrl = `/images/${img}.jpg`;
-  const altImgUrl = `/images/${altImg}.jpg`;
+    const imgUrl = `/images/${img}.jpg`;
+    const altImgUrl = `/images/${altImg}.jpg`;
 
-  useEffect(() => {
-    if (openable) {
-      const handleClickOutside = (event: MouseEvent) => {
-        if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
-          setOpen(false);
-        }
-      };
+    useEffect(() => {
+      if (openable) {
+        const handleClickOutside = (event: MouseEvent) => {
+          if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
+            setOpen(false);
+          }
+        };
 
-      document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('mousedown', handleClickOutside);
 
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
-      };
-    }
-  }, [openable]);
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }
+    }, [openable]);
 
-  return (
-    <div className="relative">
-      {isOpen && (
-        <>
-          <div className="popup-background"></div>
-          <div
-            ref={cardRef}
-            className={cn(
-              'block rounded-[40px] absolute z-50 -left-[35%] p-[11px] -top-[42%]  w-[408px] h-[647px] border-[2px] border-[#FF6CFF] bg-[#FF6CB6] p-2 shadow-card text-xl hover:bg-[#F93598] hover:border-[#B1256C] popup'
-            )}
-            onClick={() => setOpen(false)}
-          >
+    return (
+      <div className="relative">
+        {isOpen && (
+          <>
+            <div className="popup-background"></div>
             <div
+              ref={cardRef}
               className={cn(
-                'popup-content w-full rounded-[30px] flex flex-col justify-between h-full border-[2px] border-[#FF6CFF] bg-white hover:border-[#F93598] card'
+                'block rounded-[40px] absolute z-50 -left-[35%] p-[11px] -top-[42%]  w-[408px] h-[647px] border-[2px] border-[#FF6CFF] bg-[#FF6CB6] p-2 shadow-card text-xl hover:bg-[#F93598] hover:border-[#B1256C] popup'
               )}
+              onClick={() => setOpen(false)}
             >
-              <ul className="p-12 my-auto">
-                {points &&
-                  points.map((item, index) => (
-                    <li className="pb-5" key={index}>
-                      {item}
-                    </li>
-                  ))}
-              </ul>
-              <div className="font-bold text-center py-2 text-2xl">{cardTitle}</div>
+              <div
+                className={cn(
+                  'popup-content w-full rounded-[30px] flex flex-col justify-between h-full border-[2px] border-[#FF6CFF] bg-white hover:border-[#F93598] card'
+                )}
+              >
+                <ul className="p-12 my-auto">
+                  {points &&
+                    points.map((item, index) => (
+                      <li className="pb-5" key={index}>
+                        {item}
+                      </li>
+                    ))}
+                </ul>
+                <div className="font-bold text-center py-2 text-2xl">{cardTitle}</div>
+              </div>
             </div>
-          </div>
-        </>
-      )}
-
-      <div
-        className={cn(
-          'relative h-min-content rounded-[20px] border-[2px] shadow-card',
-          { 'min-w-[145px] max-w-[223px]': isMacAir},
-          { 'w-[223px] p-[6px]': size === 'normal' && !isMacAir },
-          { 'w-[321px] p-[8px]': size === 'big' && isMacAir },
-          { 'border-[#2FA2FB]': !isHovered && !isNothingHovered },
-          'hover:bg-[#008AFF] hover:border-[#396E9A] hover:border-1',
-          { 'border-[#008AFF] bg-white': !active },
-          { 'border-[#396E9A] bg-[#008AFF] border-1': active },
-          { 'border-[#2FA2FB]/[0.7]': isSecondPlan },
-          { 'border-[#2FA2FB]/[0.5]': isThirdPlan }
+          </>
         )}
-        style={{ transform: `rotate(${rotation}deg)` }}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
+
         <div
           className={cn(
-            'grid z-0 border-[2px] rounded-[15px] bg-white truncate',
-            { 'border-[#008AFF]': !isSecondPlan && !isThirdPlan },
+            'flex relative h-min-content rounded-[20px] border-[2px] shadow-card',
+            { 'w-[154px] p-[2px]': isMacAir},
+            { 'w-[223px] p-[6px]': size === 'normal' && !isMacAir },
+            { 'w-[321px] p-[8px]': size === 'big' && isMacAir },
             { 'border-[#2FA2FB]': !isHovered && !isNothingHovered },
+            'hover:bg-[#008AFF] hover:border-[#396E9A] hover:border-1',
+            { 'border-[#008AFF] bg-white': !active },
+            { 'border-[#396E9A] bg-[#008AFF] border-1': active },
             { 'border-[#2FA2FB]/[0.7]': isSecondPlan },
             { 'border-[#2FA2FB]/[0.5]': isThirdPlan }
           )}
-          onClick={openable ? () => setOpen(!isOpen) : onCardClick}
+          style={{ transform: `rotate(${rotation}deg)` }}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
         >
           <div
             className={cn(
-              { 
-                'max-w-[208px]': isMacAir,
-                'w-[208px] h-full': size === 'normal' && !isMacAir, 
-                'w-[300px] h-full': size === 'big' && !isMacAir
-              },
-              { 'opacity-[0.7]': !isHovered && !isNothingHovered }
+              'flex flex-col z-0 border-[2px] rounded-[15px] bg-white truncate',
+              { 'border-[#008AFF]': !isSecondPlan && !isThirdPlan },
+              { 'border-[#2FA2FB]': !isHovered && !isNothingHovered },
+              { 'border-[#2FA2FB]/[0.7]': isSecondPlan },
+              { 'border-[#2FA2FB]/[0.5]': isThirdPlan },
             )}
+            onClick={openable ? () => setOpen(!isOpen) : onCardClick}
           >
-            <img
-              className={cn({
-                'w-[208px] h-full transition-transform duration-300 transform hover:scale-110 inset-0':
-                  size === 'normal',
-                'w-[300px] h-full': size === 'big'
-              })}
-              src={active ? altImgUrl : imgUrl}
-              alt="cardTitle"
-            />
-          </div>
-          <div
-            className={cn(
-              'font-bold text-center cursor-pointer py-2 border-t-2 border-[#008AFF] z-20 bg-white',
-              { 'text-[#505050] border-[#2FA2FB]': !isHovered && !isNothingHovered },
-              { 'opacity-[80%] text-[#727272] !important': isSecondPlan },
-              { 'opacity-[60%] text-[#727272] !important': isThirdPlan }
-            )}
-          >
-            {cardTitle}
+            <div
+              className={cn(
+                'w-full h-full',
+                { 'opacity-[0.7]': !isHovered && !isNothingHovered }
+              )}
+            >
+              <img
+                className={cn(
+                  'h-full transition-transform duration-300 transform hover:scale-110 inset-0')}
+                src={active ? altImgUrl : imgUrl}
+                alt="cardTitle"
+              />
+            </div>
+            <div
+              className={cn(
+                'font-bold text-center cursor-pointer py-2 border-t-2 border-[#008AFF] z-20 bg-white',
+                { 'text-[#505050] border-[#2FA2FB]': !isHovered && !isNothingHovered },
+                { 'opacity-[80%] text-[#727272] !important': isSecondPlan },
+                { 'opacity-[60%] text-[#727272] !important': isThirdPlan }
+              )}
+            >
+              {cardTitle}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
