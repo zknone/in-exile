3d1cbeafc29bd6b00c93f841dd79cfb45c95a560
Streltsov Ivan <principdereva@gmail.com>
2024-05-16 19:40:22 +0200

    fix

--- src/components/menu-popup/menu-popup.tsx
+++ src/components/menu-popup/menu-popup.tsx
@@ -28,7 +28,7 @@ export default function MenuPopup(props: {
       className={cn(containerClass, 'absolute', {
         'w-[21vw]': screenSize !== 'default',
         'top-[-50vw] ': screenSize !== 'default' && state.category === 'noCategory',
-        'top-[-71vw] ': screenSize !== 'default' && state.category !== 'noCategory',
+        'top-[-71vw] ': screenSize !== 'default' && state.category !== 'noCategory' && state.menu !== 'credits',
         'top-[-72vw] ': screenSize !== 'default' && state.category !== 'noCategory' && state.menu === 'credits',
         'max-w-[409px] max-h-[650px]': screenSize === 'default',
         'top-[-970px]': screenSize === 'default' && state.category === 'noCategory',
