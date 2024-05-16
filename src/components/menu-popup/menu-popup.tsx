069d5a026375cdfe3e59e0ac258b16db15278ddd
Streltsov Ivan <principdereva@gmail.com>
2024-05-16 20:11:59 +0200

    fix

--- src/components/menu-popup/menu-popup.tsx
+++ src/components/menu-popup/menu-popup.tsx
@@ -32,8 +32,7 @@ export default function MenuPopup(props: {
         'top-[-72vw] ': screenSize !== 'default' && state.category !== 'noCategory' && state.menu === 'credits',
         'max-w-[409px] max-h-[650px]': screenSize === 'default',
         'top-[-970px]': screenSize === 'default' && state.category === 'noCategory',
-        'top-[-1370px]': screenSize === 'default' && state.category !== 'noCategory' && state.menu !== 'credits',
-        'top-[-1398px]': screenSize === 'default' && state.category !== 'noCategory' && state.menu === 'credits'
+        'top-[-1398px]': screenSize === 'default' && state.category !== 'noCategory',
       })}
     >
       <div className={cn('flex gap-2 justify-center w-full')}>
