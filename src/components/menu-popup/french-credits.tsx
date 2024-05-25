0ae74f01af9e8dc4b3dbcb1500ae00e6328c8dc5
Streltsov Ivan <principdereva@gmail.com>
2024-05-25 20:09:33 +0200

    fix: textes credits

--- src/components/menu-popup/french-credits.tsx
+++ src/components/menu-popup/french-credits.tsx
@@ -3,43 +3,27 @@ import cn from 'classnames';
 
 export default function FrenchCredits({ screenSize }: { screenSize: ScreenSize }) {
   return (
-    <>
-      <div
-        className={cn('flex flex-col', {
-          'text-[calc(4px_+_12_*_((100vw_-_375px)_/_(1600_-_375)))]': screenSize !== 'default',
-          'text-[18px]': screenSize === 'default'
-        })}
-      >
-        <dl className="block">
-          <dt
-            className={cn('font-bold', {
-              'text-[calc(6px_+_12_*_((100vw_-_375px)_/_(1600_-_375)))]': screenSize !== 'default',
-              'text-[22px]': screenSize === 'default'
-            })}
-          >
-            Judith Depaule
-          </dt>
-          <dd className="mb-[10%]">direction artistique</dd>
-          <dt
-            className={cn('font-bold', {
-              'text-[calc(6px_+_12_*_((100vw_-_375px)_/_(1600_-_375)))]': screenSize !== 'default',
-              'text-[22px]': screenSize === 'default'
-            })}
-          >
-            Alexandre Plusnine
-          </dt>
-          <dd className="mb-[10%]">design produit, design web, illustrations</dd>
-          <dt
-            className={cn('font-bold', {
-              'text-[calc(6px_+_12_*_((100vw_-_375px)_/_(1600_-_375)))]': screenSize !== 'default',
-              'text-[22px]': screenSize === 'default'
-            })}
-          >
-            Ivan Streltsov
-          </dt>
-          <dd className="mb-[10%]">développement frontend</dd>
-        </dl>
-      </div>
-    </>
+    <div
+      className={cn('flex flex-col', {
+        'text-[calc(4px_+_12_*_((100vw_-_375px)_/_(1600_-_375)))]': screenSize !== 'default',
+        'text-[18px]': screenSize === 'default'
+      })}
+    >
+      <dl className="block">
+        <dt className="font-bold">Judith Depaule</dt>
+        <dd className="mb-[4%]">direction artistique</dd>
+        <dt className="font-bold">Alexandre Plusnine</dt>
+        <dd className="mb-[4%]">design produit</dd>
+        <dt className="font-bold">Ivan Streltsov</dt>
+        <dd className="mb-[4%]">développeur frontend</dd>
+        <dt className="font-bold">Ryan Mai</dt>
+        <dd className="mb-[6%]">traduction</dd>
+        <div>
+          <a href="http://welcome-cards.aa-e.org">welcome-cards.aa-e.org</a>
+          <br />
+          <a href="mailto:contact@aa-e.org">contact@aa-e.org</a>
+        </div>
+      </dl>
+    </div>
   );
 }
